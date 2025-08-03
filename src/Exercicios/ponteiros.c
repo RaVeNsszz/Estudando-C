c09594e5c1bb1a0fbcb107dbcfe59468d9fb09e4
Rayane Rodrigues <rayane.rodrigues.1@academico.ifpb.edu.br>
2025-08-02 21:31:46 -0300

    feat: (q3) troca de valores com ponteiros

--- src/Exercicios/ponteiros.c
+++ src/Exercicios/ponteiros.c
@@ -1,16 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Função para trocar os valores de duas variáveis inteiras usando ponteiros
+void trocar(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
 int main(){
     // Exercício de Fixação
 
     /*
-    Q1
-    Escreva um programa que declare um inteiro, um real e um char, e ponteiros para inteiro, real, e char. Associe as variaveis aos ponteiros (use &). Modifique os valores de ´
-    cada variavel usando os ponteiros. Imprima os valores das vari ´ aveis antes e ap ´ os a ´
-    modificac¸ao.  
-    */
-    /*
+    Q1 - Escreva um programa que declare um inteiro, um real e um char, e ponteiros para inteiro, 
+    real e char. Associe as variaveis aos ponteiros (use &). Modifique os valores de cada variavel
+    usando os ponteiros. Imprima os valores das variaveis antes e apos a modificacao.  
+    
     int inteiro;
     float real;
     char c;
@@ -40,7 +45,7 @@ int main(){
     /*
     q2 - Escreva um programa que contenha duas variaveis inteiras. Compare seus enderecos e
     exiba o maior endereco
-    */
+    
     int a = 5, b = 10;
     int *p_a = &a;
     int *p_b = &b;
@@ -51,8 +56,16 @@ int main(){
     } else {
         printf("O maior endereco e de b: %p\n", (void*)p_b);
     }
-
-
+    */
+    /*
+    q3 - Faca um programa que leia 2 valores inteiros e chame uma funcao que receba estas 2 
+    variaveis e troque o seu conteudo, ou seja, esta funcao e chamada passando duas variaveis
+    A e B por exemplo e, apos a execucao da funcao, A contera o valor de B e B tera o valor de A.
+    */
+    int a = 5, b = 10;
+    printf("Antes da troca: A = %d, B = %d\n", a, b);
+    trocar(&a, &b);
+    printf("Depois da troca: A = %d, B = %d\n", a, b);
 
     return 0; 
 }
